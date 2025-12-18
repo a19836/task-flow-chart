9857c0386f1c06509087a4552ab941959a4071cd
JP <a19836@gmail.com>
2025-12-18 20:06:00 +0000

    update repo name

--- js/TaskFlowChart.js
+++ js/TaskFlowChart.js
@@ -4,7 +4,7 @@
  * Multi-licensed: BSD 3-Clause | Apache 2.0 | GNU LGPL v3 | HLNC License (http://bloxtor.com/LICENSE_HLNC.md)
  * Choose one license that best fits your needs.
  *
- * Original JQuery Task Flow Chart Repo: https://github.com/a19836/jquery-task-flow-chart/
+ * Original Task Flow Chart Repo: https://github.com/a19836/task-flow-chart/
  * Original Bloxtor Repo: https://github.com/a19836/bloxtor
  *
  * YOU ARE NOT AUTHORIZED TO MODIFY OR REMOVE ANY PART OF THIS NOTICE!
